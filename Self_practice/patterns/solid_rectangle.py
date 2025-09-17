399940715b3abab83987739993ac30c86b0b09a1
Aakash <legendarypowers07@gmail.com>
2025-09-17 18:38:51 +0530

    pattern 1

---
+++ Self_practice/patterns/solid_rectangle.py
@@ -0,0 +1,18 @@
+# Here we need to make a simple rectangle
+
+# ******
+# ******
+# ******
+# ******
+
+
+#  here the code goes 
+
+rows = int(input("Enter the number of rows:"))
+columns = int(input("Enter the number of columns:"))
+
+for i in range(rows):
+    for j in range(columns):
+        print("*",end="")
+
+    print()
